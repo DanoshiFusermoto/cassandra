f8c75ad8f83650ac0e653bedaf93790c03dbf34c
Dan <dan@radixdlt.com>
2020-11-21 10:58:57 +0000

    AGENT global values

--- Hackaction/src/main/java/org/fuserleer/Hackaction.java
+++ Hackaction/src/main/java/org/fuserleer/Hackaction.java
@@ -2,6 +2,12 @@ package org.fuserleer;
 
 public class Hackaction
 {
+	public static final int 	PROTOCOL_VERSION 		= 100;
+	public static final int 	AGENT_VERSION 			= 100;
+	public static final int 	REFUSE_AGENT_VERSION 	= 99;
+	public static final String 	URI_PREFIX 				= "CERBY://";
+	public static final String 	AGENT 					= URI_PREFIX+AGENT_VERSION;
+
 	public static void main(String[] args)
 	{
 	}

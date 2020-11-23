6adb54bef8541202a5752e929a3a666d07a32fe4
Dan <dan@radixdlt.com>
2020-11-23 09:21:19 +0000

    Time provider for simulated time

---
+++ Hackaction/src/main/java/org/fuserleer/time/SimulatedTime.java
@@ -0,0 +1,46 @@
+package org.fuserleer.time;
+
+import java.util.concurrent.TimeUnit;
+
+import org.fuserleer.Configuration;
+import org.fuserleer.Context;
+import org.fuserleer.Universe;
+
+public class SimulatedTime implements TimeProvider
+{
+	private long initialTime;
+	private long millisPerCommit;
+	private long highestCommit = 0;
+	
+	public SimulatedTime(Configuration configuration)
+	{
+		this.initialTime = configuration.get("time.simulated.inital", Universe.getDefault().getTimestamp());
+		this.millisPerCommit = configuration.get("time.simulated.increment", 10);
+	}
+
+	@Override
+	public long getSystemTime()
+	{
+		return System.currentTimeMillis();
+	}
+
+	@Override
+	public int getLedgerTimeSeconds()
+	{
+		this.highestCommit = Math.max(Context.get().getLedger().getHead().getHeight(), this.highestCommit);
+		return (int) TimeUnit.MILLISECONDS.toSeconds(this.initialTime + (this.highestCommit * millisPerCommit));
+	}
+
+	@Override
+	public long getLedgerTimeMS()
+	{
+		this.highestCommit = Math.max(Context.get().getLedger().getHead().getHeight(), this.highestCommit);
+		return this.initialTime + (this.highestCommit * millisPerCommit);
+	}
+
+	@Override
+	public boolean isSynchronized()
+	{
+		return false;
+	}
+}

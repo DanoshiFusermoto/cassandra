97d2f8ce23e9779e5276792a4a6c9e529e45d27f
Dan <dan@radixdlt.com>
2020-11-23 14:26:29 +0000

    Global agent fields

---
+++ Hackaction/src/main/java/org/fuserleer/common/Agent.java
@@ -0,0 +1,10 @@
+package org.fuserleer.common;
+
+public class Agent
+{
+	public static final int 	PROTOCOL_VERSION 		= 100;
+	public static final int 	AGENT_VERSION 			= 100;
+	public static final int 	REFUSE_AGENT_VERSION 	= 99;
+	public static final String 	URI_PREFIX 				= "FLEX://";
+	public static final String 	AGENT 					= URI_PREFIX+AGENT_VERSION;
+}

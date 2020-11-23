fdbc73866379e6d75204fd5fffd48b8987a8f845
Dan Hughes <dan@radixdlt.com>
2020-11-23 18:53:23 +0000

    Minimal initial LedgerStore to store & inclusion test primitives

--- core/src/main/java/org/fuserleer/ledger/Ledger.java
+++ core/src/main/java/org/fuserleer/ledger/Ledger.java
@@ -2,13 +2,18 @@ package org.fuserleer.ledger;
 
 import java.io.IOException;
 import java.util.Objects;
+import java.util.concurrent.atomic.AtomicReference;
 
 import org.fuserleer.Context;
 import org.fuserleer.Service;
+import org.fuserleer.Universe;
 import org.fuserleer.exceptions.StartupException;
 import org.fuserleer.exceptions.TerminationException;
+import org.fuserleer.exceptions.ValidationException;
 import org.fuserleer.logging.Logger;
 import org.fuserleer.logging.Logging;
+
+import com.fasterxml.jackson.annotation.JsonGetter;
 
 public final class Ledger implements Service
 {
@@ -16,34 +21,83 @@ public final class Ledger implements Service
 
 	private final Context context;
 	
-	private transient BlockHeader head;
+	private final LedgerStore ledgerStore;
+
+	private final transient AtomicReference<BlockHeader> head;
 
 	public Ledger(Context context)
 	{
 		this.context = Objects.requireNonNull(context);
+
+		this.ledgerStore = new LedgerStore(this.context);
+		this.head = new AtomicReference<BlockHeader>(this.context.getNode().getHead());
 	}
 	
 	@Override
 	public void start() throws StartupException
 	{
-		// TODO Auto-generated method stub
-		
+		try
+		{
+			this.ledgerStore.start();
+			
+			integrity();
+		}
+		catch (Exception ex)
+		{
+			throw new StartupException(ex);
+		}			
 	}
 
 	@Override
 	public void stop() throws TerminationException
 	{
-		// TODO Auto-generated method stub
-		
+		this.ledgerStore.stop();
 	}
 	
 	public void clean() throws IOException
 	{
+		this.ledgerStore.clean();
 	}
 	
+	private void integrity() throws IOException, ValidationException
+	{
+		BlockHeader nodeBlockHeader = this.context.getNode().getHead();
+		// Check if this is just a new ledger store and doesn't need integrity or recovery
+		if (nodeBlockHeader.equals(Universe.getDefault().getGenesis()) == true && this.ledgerStore.has(nodeBlockHeader.getHash()) == false)
+		{
+			// Store the genesis block primitive
+			this.ledgerStore.store(Universe.getDefault().getGenesis());
+
+			// TODO need to commit the state here but components are not ready yet
+			return;
+		}
+		else if (this.ledgerStore.has(nodeBlockHeader.getHash()) == false)
+		{
+			// TODO recover to the best head with committed state
+			ledgerLog.error(Ledger.this.context.getName()+": Local node block header "+nodeBlockHeader+" not found in store");
+			throw new UnsupportedOperationException("Integrity recovery not implemented");
+		}
+		else
+		{
+			// TODO block header is known but is it the strongest head that represents state?
+			setHead(nodeBlockHeader);
+		}
+	}
+	
+	LedgerStore getLedgerStore()
+	{
+		return this.ledgerStore;
+	}
+	
+	@JsonGetter("head")
 	public BlockHeader getHead()
 	{
-		return this.head;
+		return this.head.get();
+	}
+
+	void setHead(BlockHeader head)
+	{
+		this.head.set(Objects.requireNonNull(head));
 	}
 
 }
---
+++ core/src/main/java/org/fuserleer/ledger/LedgerStore.java
@@ -0,0 +1,206 @@
+package org.fuserleer.ledger;
+
+import java.io.IOException;
+import java.util.Objects;
+
+import org.fuserleer.Context;
+import org.fuserleer.crypto.Hash;
+import org.fuserleer.database.DatabaseException;
+import org.fuserleer.database.DatabaseStore;
+import org.fuserleer.exceptions.StartupException;
+import org.fuserleer.exceptions.TerminationException;
+import org.fuserleer.ledger.atoms.Atom;
+import org.fuserleer.logging.Logger;
+import org.fuserleer.logging.Logging;
+import org.fuserleer.serialization.DsonOutput;
+import org.fuserleer.serialization.Serialization;
+
+import com.sleepycat.je.Database;
+import com.sleepycat.je.DatabaseConfig;
+import com.sleepycat.je.DatabaseEntry;
+import com.sleepycat.je.DatabaseNotFoundException;
+import com.sleepycat.je.LockMode;
+import com.sleepycat.je.OperationStatus;
+import com.sleepycat.je.Transaction;
+import com.sleepycat.je.TransactionConfig;
+
+public class LedgerStore extends DatabaseStore
+{
+	private static final Logger databaseLog = Logging.getLogger("database");
+	
+	private final Context context;
+
+	private Database primitivesDatabase;
+
+	public LedgerStore(Context context)
+	{
+		super(Objects.requireNonNull(context.getDatabaseEnvironment()));
+		this.context = context;
+		
+		// GOT IT!
+		databaseLog.setLevels(Logging.ERROR | Logging.FATAL | Logging.INFO | Logging.WARN | Logging.WARN);
+	}
+
+	@Override
+	public void start() throws StartupException
+	{
+		try
+		{
+			if (this.context.getConfiguration().getCommandLine().hasOption("clean") == true)
+				clean();
+				
+			DatabaseConfig primitivesConfig = new DatabaseConfig();
+			primitivesConfig.setAllowCreate(true);
+			primitivesConfig.setTransactional(true);
+			primitivesConfig.setKeyPrefixing(true);
+
+			Transaction transaction = null;
+			try
+			{
+				transaction = getEnvironment().beginTransaction(null, new TransactionConfig().setReadUncommitted(true));
+
+				this.primitivesDatabase = getEnvironment().openDatabase(transaction, "hackation.primitives", primitivesConfig);
+				
+				transaction.commit();
+			}
+			catch (DatabaseNotFoundException dsnfex)
+			{
+				if (transaction != null)
+					transaction.abort();
+
+				log.warn(dsnfex.getMessage());
+			}
+			catch (Exception ex)
+			{
+				if (transaction != null)
+					transaction.abort();
+
+				throw new DatabaseException(ex);
+			}
+		}
+		catch (IOException ioex)
+		{
+			throw new StartupException(ioex);
+		}
+	}
+
+	@Override
+	public void stop() throws TerminationException
+	{
+		try
+		{
+			close();
+		}
+		catch (IOException ioex)
+		{
+			throw new TerminationException(ioex);
+		}
+	}	
+
+	@Override
+	public void close() throws IOException
+	{
+		super.close();
+
+		if (this.primitivesDatabase != null) this.primitivesDatabase.close();
+	}
+	
+	@Override
+	public void clean() throws DatabaseException
+	{
+		Transaction transaction = null;
+
+		try
+		{
+			transaction = getEnvironment().beginTransaction(null, new TransactionConfig().setReadUncommitted(true));
+			getEnvironment().truncateDatabase(transaction, "hackation.primitives", false);
+
+			transaction.commit();
+		}
+		catch (DatabaseNotFoundException dsnfex)
+		{
+			if (transaction != null)
+				transaction.abort();
+
+			log.warn(dsnfex.getMessage());
+		}
+		catch (Exception ex)
+		{
+			if (transaction != null)
+				transaction.abort();
+
+			throw new DatabaseException(ex);
+		}
+	}
+
+	@Override
+	public void flush() throws DatabaseException  { /* Not used */ }
+
+	boolean has(Hash hash) throws DatabaseException
+	{
+		try
+        {
+			OperationStatus status = this.primitivesDatabase.get(null, new DatabaseEntry(hash.toByteArray()), null, LockMode.DEFAULT);
+			if (status.equals(OperationStatus.SUCCESS) == true)
+				return true;
+
+			return false;
+        }
+		catch (Throwable t)
+		{
+			if (t instanceof DatabaseException)
+				throw (DatabaseException)t;
+			else
+				throw new DatabaseException(t);
+		}
+	}
+	
+	final OperationStatus store(Block block) throws IOException 
+	{
+		Transaction transaction = this.context.getDatabaseEnvironment().beginTransaction(null, null);
+		try 
+		{
+			BlockHeader blockHeader = block.toHeader();
+			OperationStatus status = store(transaction, block.getHash(), blockHeader, Serialization.getInstance().toDson(blockHeader, DsonOutput.Output.PERSIST));
+		    if (status.equals(OperationStatus.SUCCESS) == false) 
+		    {
+		    	if (status.equals(OperationStatus.KEYEXIST) == true) 
+		    		databaseLog.warn(this.context.getName()+": Block header " + blockHeader + " is already present");
+		    	else 
+		    		throw new DatabaseException("Failed to store " + blockHeader.getHash() + " due to " + status.name());
+		    } 
+		    
+		    for (Atom atom : block.getAtoms())
+		    {
+				status = store(transaction, atom.getHash(), atom, Serialization.getInstance().toDson(atom, DsonOutput.Output.PERSIST));
+			    if (status.equals(OperationStatus.SUCCESS) == false) 
+			    {
+			    	if (status.equals(OperationStatus.KEYEXIST) == true) 
+			    		databaseLog.warn(this.context.getName()+": Atom "+atom.getHash()+" in block "+blockHeader + " is already present");
+			    	else 
+			    		throw new DatabaseException("Failed to store atom "+atom.getHash()+" in block "+blockHeader + " due to " + status.name());
+			    } 
+		    }
+		    
+		    transaction.commit();
+		    return OperationStatus.SUCCESS;
+		} 
+		catch (Exception ex) 
+		{
+			transaction.abort();
+		    if (ex instanceof DatabaseException)
+		    	throw ex; 
+		    throw new DatabaseException(ex);
+		} 
+	}
+	
+	private OperationStatus store(Transaction transaction, Hash hash, Primitive primitive, byte[] bytes) throws IOException 
+	{
+		Objects.requireNonNull(hash);
+		Objects.requireNonNull(bytes);
+		DatabaseEntry key = new DatabaseEntry(hash.toByteArray());
+		DatabaseEntry value = new DatabaseEntry(bytes);
+		OperationStatus status = this.primitivesDatabase.putNoOverwrite(transaction, key, value);
+		return status;
+	}
+}

ce9acee9524710777f634348c119185f98ccf508
Dan Hughes <dan@radixdlt.com>
2020-11-24 11:51:07 +0000

    Added block header to atom committed event

--- core/src/main/java/org/fuserleer/ledger/events/AtomCommittedEvent.java
+++ core/src/main/java/org/fuserleer/ledger/events/AtomCommittedEvent.java
@@ -1,11 +1,23 @@
 package org.fuserleer.ledger.events;
 
+import java.util.Objects;
+
+import org.fuserleer.ledger.BlockHeader;
 import org.fuserleer.ledger.atoms.Atom;
 
 public final class AtomCommittedEvent extends AtomEvent 
 {
-	public AtomCommittedEvent(Atom atom)
+	private final BlockHeader blockHeader;
+	
+	public AtomCommittedEvent(BlockHeader blockHeader, Atom atom)
 	{
 		super(atom);
+		
+		this.blockHeader = Objects.requireNonNull(blockHeader);
+	}
+	
+	public BlockHeader getBlockHeader()
+	{
+		return this.blockHeader;
 	}
 }

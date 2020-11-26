4d1a85fbc7f981f6f46bd55c09a562f9b1e5ff2f
Dan Hughes <dan@radixdlt.com>
2020-11-26 15:56:51 +0000

    Crude bag of signatures object

---
+++ core/src/main/java/org/fuserleer/crypto/ECSignatureBag.java
@@ -0,0 +1,57 @@
+package org.fuserleer.crypto;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.LinkedHashSet;
+import java.util.Map;
+import java.util.Objects;
+
+import org.fuserleer.serialization.DsonOutput;
+import org.fuserleer.serialization.SerializerConstants;
+import org.fuserleer.serialization.SerializerDummy;
+import org.fuserleer.serialization.SerializerId2;
+import org.fuserleer.serialization.DsonOutput.Output;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+
+@SerializerId2("crypto.ecsignature_bag")
+public final class ECSignatureBag
+{
+	// Placeholder for the serializer ID
+	@JsonProperty(SerializerConstants.SERIALIZER_TYPE_NAME)
+	@DsonOutput(Output.ALL)
+	private SerializerDummy serializer = SerializerDummy.DUMMY;
+
+	@JsonProperty("signatures")
+	@DsonOutput(value = {Output.API, Output.WIRE, Output.PERSIST})
+	@JsonDeserialize(as=LinkedHashMap.class)
+	private Map<ECPublicKey, ECSignature> signatures;
+	
+	private ECSignatureBag()
+	{
+		// For serializer
+	}
+	
+	public ECSignatureBag(Map<ECPublicKey, ECSignature> signatures)
+	{
+		this.signatures = new LinkedHashMap<ECPublicKey, ECSignature>(signatures);
+	}
+	
+	public Collection<ECPublicKey> getSigners()
+	{
+		return Collections.unmodifiableCollection(this.signatures.keySet());
+	}
+	
+	public boolean verify(Hash hash, ECPublicKey signer)
+	{
+		Objects.requireNonNull(hash, "Hash to verify is null");
+		Objects.requireNonNull(signer, "Signer is null");
+		
+		if (this.signatures.containsKey(signer) == false)
+			return false;
+		
+		return signer.verify(hash, this.signatures.get(signer));
+	}
+}

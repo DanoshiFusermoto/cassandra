bfd7838a702c7c566a6025b155f7c415165e9dfe
Dan Hughes <dan@radixdlt.com>
2020-12-03 15:18:58 +0000

    Change BlockVote from carrying a header to a hash

--- core/src/main/java/org/fuserleer/ledger/BlockVote.java
+++ core/src/main/java/org/fuserleer/ledger/BlockVote.java
@@ -1,18 +1,19 @@
 package org.fuserleer.ledger;
 
 import org.fuserleer.crypto.ECPublicKey;
+import org.fuserleer.crypto.Hash;
 import org.fuserleer.serialization.SerializerId2;
 
 // FIXME needs to be a Hash, not block header, then use the inventory mechanism to fetch the block header if missing locally
 @SerializerId2("ledger.vote.block")
-public final class BlockVote extends Vote<BlockHeader>
+public final class BlockVote extends Vote<Hash>
 {
 	private BlockVote()
 	{
 		// SERIALIZER
 	}
 	
-	public BlockVote(final BlockHeader object, final long clock, final ECPublicKey owner)
+	public BlockVote(final Hash object, final long clock, final ECPublicKey owner)
 	{
 		super(object, clock, owner);
 	}

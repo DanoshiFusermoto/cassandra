ef83a1f1076c59093efad30c2a2e1532a99bd1e8
Dan Hughes <dan@radixdlt.com>
2020-12-24 14:23:32 +0000

    API changes for new inventory method

--- core/src/main/java/org/fuserleer/API.java
+++ core/src/main/java/org/fuserleer/API.java
@@ -18,6 +18,7 @@ import org.fuserleer.exceptions.StartupException;
 import org.fuserleer.exceptions.TerminationException;
 import org.fuserleer.ledger.Block;
 import org.fuserleer.ledger.BlockHeader;
+import org.fuserleer.ledger.BlockHeader.InventoryType;
 import org.fuserleer.ledger.SearchQuery;
 import org.fuserleer.ledger.SearchResponse;
 import org.fuserleer.ledger.atoms.Atom;
@@ -141,7 +142,7 @@ public class API implements Service
 						throw new IllegalArgumentException("Limit is greater than 100");
 
 					Block block = API.this.context.getLedger().get(API.this.context.getLedger().getHead().getHash(), Block.class);
-					long offset = block.getHeader().getIndex()+block.getHeader().getInventory(Atom.class).size();
+					long offset = block.getHeader().getIndex()+block.getHeader().getInventory(InventoryType.ATOMS).size();
 					long nextOffset = offset;
 					JSONArray headersArray = new JSONArray();
 					JSONArray atomsArray = new JSONArray();
@@ -186,7 +187,7 @@ public class API implements Service
 					@SuppressWarnings("unchecked")
 					Class<? extends Particle> container = (Class<? extends Particle>) Serialization.getInstance().getClassForId(req.params("container").toLowerCase());
 					Block block = API.this.context.getLedger().get(API.this.context.getLedger().getHead().getHash(), Block.class);
-					long offset = block.getHeader().getIndex()+block.getHeader().getInventory(Atom.class).size();
+					long offset = block.getHeader().getIndex()+block.getHeader().getInventory(InventoryType.ATOMS).size();
 					long nextOffset = offset;
 					JSONArray headersArray = new JSONArray();
 					JSONArray atomsArray = new JSONArray();

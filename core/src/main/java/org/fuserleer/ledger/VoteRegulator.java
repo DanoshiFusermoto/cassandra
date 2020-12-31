6e8a55f8b6fa7d3110d8c5d99fa063e31c330873
Dan Hughes <dan@radixdlt.com>
2020-12-31 11:18:40 +0000

    Converted VoteReg to a service, added bloom processing and sharding

--- core/src/main/java/org/fuserleer/ledger/VoteRegulator.java
+++ core/src/main/java/org/fuserleer/ledger/VoteRegulator.java
@@ -2,80 +2,99 @@ package org.fuserleer.ledger;
 
 import java.util.Collections;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Objects;
 import java.util.Set;
 
 import org.fuserleer.Context;
+import org.fuserleer.Service;
 import org.fuserleer.Universe;
 import org.fuserleer.crypto.ECPublicKey;
+import org.fuserleer.events.SynchronousEventListener;
+import org.fuserleer.exceptions.StartupException;
+import org.fuserleer.exceptions.TerminationException;
+import org.fuserleer.ledger.atoms.AtomCertificate;
+import org.fuserleer.ledger.atoms.ParticleCertificate;
+import org.fuserleer.ledger.events.BlockCommittedEvent;
 import org.fuserleer.logging.Logger;
 import org.fuserleer.logging.Logging;
+import org.fuserleer.utils.UInt128;
 
-public final class VoteRegulator
+import com.google.common.eventbus.Subscribe;
+
+public final class VoteRegulator implements Service
 {
-	private static final Logger ledgerLog = Logging.getLogger("ledger");
+	private static final Logger powerLog = Logging.getLogger("power");
 
 	private final Context context;
-	private final Map<Long, Long> totalVotePower;
 	private final Map<ECPublicKey, Long> votePower;
 	
 	VoteRegulator(final Context context)
 	{
 		this.context = Objects.requireNonNull(context, "Context is null");
 		this.votePower = Collections.synchronizedMap(new HashMap<ECPublicKey, Long>());
-		this.totalVotePower = Collections.synchronizedMap(new HashMap<Long, Long>());
-		for (ECPublicKey genode : Universe.getDefault().getGenodes())
-			this.votePower.put(genode, 1l);
-		this.totalVotePower.put(0l, (long) Universe.getDefault().getGenodes().size());
 	}
 	
-	public long getVotePower(ECPublicKey identity, long height)
+	@Override
+	public void start() throws StartupException
 	{
-		return getVotePower(identity, height, Collections.emptyList());
+		this.votePower.clear();
+		for (ECPublicKey genode : Universe.getDefault().getGenodes())
+		{
+			powerLog.info(this.context.getName()+": Setting vote power for genesis node "+genode+":"+this.context.getLedger().getShardGroup(genode)+" to "+1);
+			this.votePower.put(genode, 1l);
+		}
+		
+		this.context.getEvents().register(this.syncBlockListener);
 	}
 
-	long getVotePower(ECPublicKey identity, long height, List<BlockHeader> pending)
+	@Override
+	public void stop() throws TerminationException
+	{
+		this.context.getEvents().unregister(this.syncBlockListener);
+	}
+	
+	public long getVotePower(ECPublicKey identity)
 	{
 		return this.votePower.getOrDefault(identity, 0l);
 	}
 	
-	public VotePowerBloom getVotePowerBloom(long height)
+	public VotePowerBloom getVotePowerBloom(UInt128 shardGroup)
 	{
-		if (height < 0)
-			throw new IllegalArgumentException("Height is negative");
-
-		VotePowerBloom votePowerBloom = new VotePowerBloom(height);
+		VotePowerBloom votePowerBloom = new VotePowerBloom();
 		synchronized(this.votePower)
 		{
 			for (Entry<ECPublicKey, Long> votePower : this.votePower.entrySet())
+			{
+				if (this.context.getLedger().getShardGroup(votePower.getKey()).compareTo(shardGroup) != 0)
+					continue;
+				
 				votePowerBloom.add(votePower.getKey(), votePower.getValue());
+			}
 		}
 
 		return votePowerBloom;
 	}
 
-	public long getTotalVotePower(long height)
+	public long getTotalVotePower(Set<UInt128> shardGroups)
 	{
-		if (height < 0)
-			throw new IllegalArgumentException("Height is negative");
-		
-		if (height > this.context.getLedger().getHead().getHeight())
-			height = this.context.getLedger().getHead().getHeight();
+		synchronized(this.votePower)
+		{
+			long totalVotePower = 0;
+			for (Entry<ECPublicKey, Long> votePower : this.votePower.entrySet())
+			{
+				if (shardGroups.contains(this.context.getLedger().getShardGroup(votePower.getKey())) == false)
+					continue;
 
-		return this.totalVotePower.getOrDefault(height, 0l);
+				totalVotePower += votePower.getValue(); 
+			}
+			return totalVotePower;
+		}
 	}
 	
-	public long getVotePower(long height, Set<ECPublicKey> identities)
+	public long getVotePower(Set<ECPublicKey> identities)
 	{
-		if (height < 0)
-			throw new IllegalArgumentException("Height is negative");
-		
-		if (height > this.context.getLedger().getHead().getHeight())
-			height = this.context.getLedger().getHead().getHeight();
-
 		long votePower = 0l;
 		for (ECPublicKey identity : identities)
 			votePower += this.votePower.getOrDefault(identity, 0l);
@@ -83,23 +102,15 @@ public final class VoteRegulator
 		return votePower;
 	}
 
-	public long getVotePowerThreshold(long height)
+	public long getVotePowerThreshold(Set<UInt128> shardGroups)
 	{
-		return twoFPlusOne(getTotalVotePower(height));
+		return twoFPlusOne(getTotalVotePower(shardGroups));
 	}
 	
-	long addVotePower(ECPublicKey identity, long height)
+	long addVotePower(ECPublicKey identity)
 	{
-		if (height < 0)
-			throw new IllegalArgumentException("Height is negative");
-		
-		if (height > this.context.getLedger().getHead().getHeight())
-			height = this.context.getLedger().getHead().getHeight();
-
 		this.votePower.put(identity, this.votePower.getOrDefault(identity, 0l) + 1l);
-		this.totalVotePower.put(height, this.totalVotePower.getOrDefault(height-1, 0l) + 1l);
-		
-		ledgerLog.info("Incrementing vote power for "+identity+"@"+height+" to "+this.votePower.get(identity)+" total vote power "+this.totalVotePower.get(height));
+		powerLog.info(this.context.getName()+": Incrementing vote power for "+identity+":"+this.context.getLedger().getShardGroup(identity)+" to "+this.votePower.get(identity));
 		
 		return this.votePower.getOrDefault(identity, 0l);
 	}
@@ -110,4 +121,34 @@ public final class VoteRegulator
 		long T = F * 2;
 		return T + 1;
 	}
+	
+	private void processVoteBloom(Set<ECPublicKey> identities, VotePowerBloom votePowerBloom)
+	{
+		synchronized(this.votePower)
+		{
+			for (ECPublicKey identity : identities)
+			{
+				long power = votePowerBloom.power(identity);
+				if (power > this.votePower.getOrDefault(identity, 0l))
+				{
+					this.votePower.put(identity, power);
+					powerLog.info(this.context.getName()+": Setting vote power for "+identity+":"+this.context.getLedger().getShardGroup(identity)+" to "+power);
+				}
+			}
+		}
+	}
+	
+	// BLOCK LISTENER //
+	private SynchronousEventListener syncBlockListener = new SynchronousEventListener()
+	{
+		@Subscribe
+		public void on(BlockCommittedEvent blockCommittedEvent) 
+		{
+			for (AtomCertificate atomCertificate : blockCommittedEvent.getBlock().getCertificates())
+			{
+				for (ParticleCertificate particleCertificate : atomCertificate.getAll())
+					processVoteBloom(particleCertificate.getSignatures().getSigners(), particleCertificate.getVotePowers());
+			}
+		}
+	};
 }

c8e2be270ed74e5da3f873d31b4cf47393c6bd1b
Dan Hughes <dan@radixdlt.com>
2020-12-31 11:21:03 +0000

    Minor change to support new sync signal method

--- core/src/main/java/org/fuserleer/console/Ledger.java
+++ core/src/main/java/org/fuserleer/console/Ledger.java
@@ -23,7 +23,7 @@ public class Ledger extends Function
 		CommandLine commandLine = Function.parser.parse(options, arguments);
 
 		{
-			printStream.println("Synced: "+context.getLedger().isInSync());
+			printStream.println("Synced: "+context.getLedger().isSynced());
 			printStream.println("Identity: "+context.getNode().getIdentity());
 			printStream.println("Current head: "+context.getLedger().getHead());
 			printStream.println("Ledger timestamp: "+Time.getLedgerTimeSeconds()+" / "+new Date(Time.getLedgerTimeMS())); // TODO only accurate for simulated time

c8829f89574da785012c706360359bdb824fcc84
Dan Hughes <dan@radixdlt.com>
2021-01-02 16:56:59 +0000

    Removed fields and added StateOps cache

--- core/src/main/java/org/fuserleer/ledger/CommitOperation.java
+++ core/src/main/java/org/fuserleer/ledger/CommitOperation.java
@@ -5,23 +5,24 @@ import java.util.HashSet;
 import java.util.Set;
 import java.util.Objects;
 
-import org.fuserleer.database.Fields;
+import org.fuserleer.crypto.Hash;
 import org.fuserleer.database.Identifier;
 import org.fuserleer.database.Indexable;
 import org.fuserleer.ledger.atoms.Atom;
 import org.fuserleer.time.Time;
 
-public class StateOperation
+public class CommitOperation
 {
 	private final BlockHeader	head;
 	private final Atom			atom;
 	private final long 			timestamp;
 	
 	private transient CommitState		state;
+	private transient Set<StateOp> 		stateOps = null;
 	private transient Set<Indexable> 	indexables = null;
 	private transient Set<Identifier> 	identifiers = null;
 
-	StateOperation(BlockHeader head, Atom atom, CommitState state)
+	CommitOperation(BlockHeader head, Atom atom, CommitState state)
 	{
 		this.head = Objects.requireNonNull(head);
 		this.atom = Objects.requireNonNull(atom);
@@ -53,7 +54,23 @@ public class StateOperation
 	{
 		return this.atom;
 	}
+	
+	public Set<Hash> getStates()
+	{
+		return this.atom.getStates();
+	}
 
+	public synchronized Set<StateOp> getStateOps() 
+	{
+		if (this.stateOps == null)
+		{
+			Set<StateOp> stateOps = new HashSet<StateOp>(this.atom.getStateOps()); 
+			this.stateOps = Collections.unmodifiableSet(stateOps);
+		}
+		
+		return this.stateOps;
+	}
+	
 	public synchronized Set<Indexable> getIndexables() 
 	{
 		if (this.indexables == null)
@@ -77,11 +94,6 @@ public class StateOperation
 		return this.identifiers;
 	}
 
-	public Fields getFields() 
-	{
-		return this.atom.getFields();
-	}
-
 	@Override
 	public int hashCode() 
 	{
@@ -97,12 +109,12 @@ public class StateOperation
 		if (object == this)
 			return true;
 		
-		if (object instanceof StateOperation)
+		if (object instanceof CommitOperation)
 		{
-			if (this.head.equals(((StateOperation)object).getHead()) == false)
+			if (this.head.equals(((CommitOperation)object).getHead()) == false)
 				return false;
 			
-			if (this.atom.equals(((StateOperation)object).getAtom()) == false)
+			if (this.atom.equals(((CommitOperation)object).getAtom()) == false)
 				return false;
 
 			return true;
@@ -116,5 +128,4 @@ public class StateOperation
 	{
 		return this.state+" "+this.head.getHash()+" "+this.atom.getHash()+" ["+getIndexables().toString()+"] ("+(getIdentifiers() == null ? "" : getIdentifiers())+")";
 	}
-
 }

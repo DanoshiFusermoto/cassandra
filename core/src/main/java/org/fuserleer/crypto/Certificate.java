7e5d0e9c129b636d11924a129544a34e04610df7
Dan Hughes <dan@radixdlt.com>
2021-03-04 12:29:11 +0000

    Minor changes to certificate object to include state decision

--- core/src/main/java/org/fuserleer/crypto/Certificate.java
+++ core/src/main/java/org/fuserleer/crypto/Certificate.java
@@ -5,7 +5,7 @@ import java.util.Objects;
 
 import org.fuserleer.BasicObject;
 import org.fuserleer.common.Primitive;
-import org.fuserleer.database.IndexablePrimitive;
+import org.fuserleer.ledger.StateDecision;
 import org.fuserleer.serialization.DsonOutput;
 import org.fuserleer.serialization.SerializerId2;
 import org.fuserleer.serialization.DsonOutput.Output;
@@ -13,14 +13,14 @@ import org.fuserleer.serialization.DsonOutput.Output;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 @SerializerId2("crypto.certificate")
-public abstract class Certificate extends BasicObject implements IndexablePrimitive
+public abstract class Certificate extends BasicObject implements Primitive
 {
 	@JsonProperty("decision")
 	@DsonOutput(Output.ALL)
-	private boolean decision;
+	private StateDecision decision;
 
 	@JsonProperty("signatures")
-	@DsonOutput(Output.ALL)
+	@DsonOutput(value = {Output.API, Output.WIRE, Output.PERSIST})
 	private ECSignatureBag	signatures;
 
 	protected Certificate()
@@ -28,31 +28,42 @@ public abstract class Certificate extends BasicObject implements IndexablePrimit
 		// For serializer
 	}
 	
-	protected Certificate(final boolean decision)
+	protected Certificate(final StateDecision decision)
 	{
 		this.signatures = new ECSignatureBag();
 		this.decision = decision;
 	}
 
-	protected Certificate(final boolean decision, final Map<ECPublicKey, ECSignature> signatures)
+	protected Certificate(final StateDecision decision, final Map<ECPublicKey, ECSignature> signatures)
+	{
+		this(decision, new ECSignatureBag(signatures));
+	}
+
+	protected Certificate(final StateDecision decision, final ECSignatureBag signatures)
 	{
 		Objects.requireNonNull(signatures, "Signatures is null");
 		if (signatures.isEmpty() == true)
 			throw new IllegalArgumentException("Signatures is empty");
 
-		this.signatures = new ECSignatureBag(signatures);
-		this.decision = decision;
+		this.decision = Objects.requireNonNull(decision, "Decision is null");
+		if (decision.equals(StateDecision.ERROR) == true || decision.equals(StateDecision.UNKNOWN) == true)
+			throw new IllegalArgumentException("Decision "+decision+" is unsupported at present");
+
+		this.signatures = signatures;
 	}
 	
 	protected final boolean add(final ECPublicKey identity, final ECSignature signature) throws CryptoException
 	{
+		Objects.requireNonNull(identity, "Identity is null");
+		Objects.requireNonNull(signature, "Signature is null");
+		
 		if (verify(identity, signature) == false)
 			throw new CryptoException("Signature from "+identity+" does not sign certificate");
 		
 		return this.signatures.add(identity, signature);
 	}
 
-	public final boolean getDecision()
+	public final StateDecision getDecision()
 	{
 		return this.decision;
 	}
@@ -66,8 +77,12 @@ public abstract class Certificate extends BasicObject implements IndexablePrimit
 	
 	public abstract boolean verify(final ECPublicKey identity, final ECSignature signature);
 
-	final boolean verify(Hash hash, ECPublicKey identity)
+	final boolean verify(final Hash hash, final ECPublicKey identity)
 	{
+		Objects.requireNonNull(identity, "Identity is null");
+		Objects.requireNonNull(hash, "Hash is null");
+		Hash.notZero(hash, "Hash is ZERO");
+
 		return this.signatures.verify(hash, identity);
 	}
 }

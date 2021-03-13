ba5c66adc14fb9844932114c94990208c529ef5c
Dan Hughes <dan@radixdlt.com>
2021-03-13 16:31:24 +0000

    Duplicated functions for int

--- core/src/main/java/org/fuserleer/utils/Numbers.java
+++ core/src/main/java/org/fuserleer/utils/Numbers.java
@@ -2,6 +2,82 @@ package org.fuserleer.utils;
 
 public class Numbers
 {
+	// INTS //
+	public static void inRange(int value, int low, int high, String message)
+	{
+		int rl = low, rh = high;
+		if (low > high)
+		{
+			rl = high;
+			rh = low;
+		}
+			
+		
+		if (value < rl || value > rh)
+		{
+			if (message == null)
+				throw new IllegalArgumentException();
+			else
+				throw new IllegalArgumentException(message);
+		}
+	}
+
+	public static void notNegative(int value, String message)
+	{
+		if (value < 0)
+		{
+			if (message == null)
+				throw new IllegalArgumentException();
+			else
+				throw new IllegalArgumentException(message);
+		}
+	}
+	
+	public static void notPositive(int value, String message)
+	{
+		if (value > 0)
+		{
+			if (message == null)
+				throw new IllegalArgumentException();
+			else
+				throw new IllegalArgumentException(message);
+		}
+	}
+
+	public static void notZero(int  value, String message)
+	{
+		if (value == 0)
+		{
+			if (message == null)
+				throw new IllegalArgumentException();
+			else
+				throw new IllegalArgumentException(message);
+		}
+	}
+
+	public static void lessThan(int  value, int  bound, String message)
+	{
+		if (value < bound)
+		{
+			if (message == null)
+				throw new IllegalArgumentException();
+			else
+				throw new IllegalArgumentException(message);
+		}
+	}
+
+	public static void greaterThan(int  value, int  bound, String message)
+	{
+		if (value > bound)
+		{
+			if (message == null)
+				throw new IllegalArgumentException();
+			else
+				throw new IllegalArgumentException(message);
+		}
+	}
+	
+	// LONGS //
 	public static void inRange(long value, long low, long high, String message)
 	{
 		long rl = low, rh = high;
@@ -75,4 +151,5 @@ public class Numbers
 				throw new IllegalArgumentException(message);
 		}
 	}
+
 }

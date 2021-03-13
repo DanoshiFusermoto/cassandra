4b77fa0a8fa4088dd04eb9640af75b90ac0ffd57
Dan Hughes <dan@radixdlt.com>
2021-03-13 14:52:56 +0000

    Legacy native twitter user particle

--- core/src/main/java/org/fuserleer/apps/twitter/TwitterUserRegistration.java
+++ core/src/main/java/org/fuserleer/apps/twitter/TwitterUserRegistration.java
@@ -2,25 +2,36 @@ package org.fuserleer.apps.twitter;
 
 import java.io.IOException;
 import java.util.Objects;
-import java.util.Set;
 
 import org.fuserleer.Universe;
 import org.fuserleer.crypto.ECPublicKey;
-import org.fuserleer.database.Indexable;
+import org.fuserleer.crypto.Hash;
 import org.fuserleer.exceptions.ValidationException;
+import org.fuserleer.ledger.StateAddress;
 import org.fuserleer.ledger.StateMachine;
+import org.fuserleer.ledger.StateOp;
+import org.fuserleer.ledger.StateOp.Instruction;
 import org.fuserleer.ledger.atoms.SignedParticle;
 import org.fuserleer.serialization.DsonOutput;
 import org.fuserleer.serialization.DsonOutput.Output;
 import org.fuserleer.serialization.SerializerId2;
+import org.fuserleer.utils.Numbers;
+import org.fuserleer.utils.UInt256;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.google.common.primitives.Longs;
 
 @SerializerId2("apps.twitter.user")
 public class TwitterUserRegistration extends SignedParticle
 {
+	public static int MIN_NAME_LENGTH = 3;
+	public static int MIN_HANDLE_LENGTH = 3;
+	public static int MIN_LOCATION_LENGTH = 3;
+	public static int MIN_DESCRIPTION_LENGTH = 3;
+	public static int MIN_URL_LENGTH = 3;
+
 	public static int MAX_NAME_LENGTH = 128;
-	public static int MAX_SCREENNAME_LENGTH = 48;
+	public static int MAX_HANDLE_LENGTH = 48;
 	public static int MAX_LOCATION_LENGTH = 256;
 	public static int MAX_DESCRIPTION_LENGTH = 256;
 	public static int MAX_URL_LENGTH = 256;
@@ -68,19 +79,16 @@ public class TwitterUserRegistration extends SignedParticle
 		super();
 	}
 	
-	public TwitterUserRegistration(long id, String handle, long createdAt, ECPublicKey owner)
+	public TwitterUserRegistration(final long id, final String handle, final long createdAt, final ECPublicKey owner)
 	{
 		super(Spin.UP, owner);
 		
 		this.id = id;
 
-		this.handle = Objects.requireNonNull(handle).toLowerCase();
-		if (this.handle.isEmpty() || this.handle.length() > TwitterUserRegistration.MAX_SCREENNAME_LENGTH)
-			throw new IllegalArgumentException("Handle "+this.handle+" length is greater than MAX_NAME_LENGTH "+TwitterUserRegistration.MAX_SCREENNAME_LENGTH);
+		setHandle(handle);
 		
 		this.createdAt = createdAt;
-		if (this.createdAt <= Universe.getDefault().getTimestamp())
-			throw new IllegalArgumentException("Created time "+this.createdAt+" is before genesis time "+Universe.getDefault().getTimestamp());
+		Numbers.lessThan(this.createdAt, Universe.getDefault().getTimestamp(), "Created time "+this.createdAt+" is before genesis time "+Universe.getDefault().getTimestamp());
 	}
 
 	public long getID() 
@@ -92,13 +100,15 @@ public class TwitterUserRegistration extends SignedParticle
 	@DsonOutput(Output.ALL)
 	public String getHandle() 
 	{
-		return this.handle.toLowerCase();
+		return this.handle.trim().toLowerCase();
 	}
 	
 	@JsonProperty("handle")
-	private void setHandle(String handle)
+	private void setHandle(final String handle)
 	{
-		this.handle = handle.toLowerCase();
+		String trimmedHandle = Objects.requireNonNull(handle, "Handle is null").trim().toLowerCase();
+		Numbers.inRange(trimmedHandle.length(), TwitterUserRegistration.MIN_HANDLE_LENGTH, TwitterUserRegistration.MAX_HANDLE_LENGTH, "Handle "+trimmedHandle+" length "+trimmedHandle.length()+" is not in range "+TwitterUserRegistration.MIN_HANDLE_LENGTH+" -> "+TwitterUserRegistration.MAX_HANDLE_LENGTH);
+		this.handle = trimmedHandle;
 	}
 
 	public long getCreatedAt() 
@@ -111,14 +121,10 @@ public class TwitterUserRegistration extends SignedParticle
 		return this.name;
 	}
 
-	public void setName(String name) 
+	public void setName(final String name) 
 	{
-		if (Objects.requireNonNull(name).isEmpty() == true)
-			return;
-
-		if (Objects.requireNonNull(name).length() > TwitterUserRegistration.MAX_NAME_LENGTH)
-			throw new IllegalArgumentException("Name "+name+" is greater than MAX_NAME_LENGTH "+TwitterUserRegistration.MAX_NAME_LENGTH);
-
+		Objects.requireNonNull(name, "Name is null");
+		Numbers.inRange(name.length(), TwitterUserRegistration.MIN_NAME_LENGTH, TwitterUserRegistration.MIN_NAME_LENGTH, "Name "+name+" length "+name.length()+" is not in range "+TwitterUserRegistration.MIN_NAME_LENGTH+" -> "+TwitterUserRegistration.MAX_NAME_LENGTH);
 		this.name = name;
 	}
 
@@ -127,14 +133,10 @@ public class TwitterUserRegistration extends SignedParticle
 		return this.location;
 	}
 
-	public void setLocation(String location) 
+	public void setLocation(final String location) 
 	{
-		if (Objects.requireNonNull(location).isEmpty() == true)
-			return;
-
-		if (Objects.requireNonNull(location).length() > TwitterUserRegistration.MAX_LOCATION_LENGTH)
-			throw new IllegalArgumentException("Location "+location+" is greater than MAX_LOCATION_LENGTH "+TwitterUserRegistration.MAX_LOCATION_LENGTH);
-
+		Objects.requireNonNull(location, "Location is null");
+		Numbers.inRange(location.length(), TwitterUserRegistration.MIN_LOCATION_LENGTH, TwitterUserRegistration.MAX_LOCATION_LENGTH, "Location "+location+" length "+location.length()+" is not in range "+TwitterUserRegistration.MIN_LOCATION_LENGTH+" -> "+TwitterUserRegistration.MAX_LOCATION_LENGTH);
 		this.location = location;
 	}
 
@@ -143,14 +145,10 @@ public class TwitterUserRegistration extends SignedParticle
 		return this.URL;
 	}
 
-	public void setURL(String URL) 
+	public void setURL(final String URL) 
 	{
-		if (Objects.requireNonNull(URL).isEmpty() == true)
-			return;
-
-		if (Objects.requireNonNull(URL).length() > TwitterUserRegistration.MAX_URL_LENGTH)
-			throw new IllegalArgumentException("URL "+URL+" length is greater than MAX_URL_LENGTH "+TwitterUserRegistration.MAX_URL_LENGTH);
-
+		Objects.requireNonNull(URL, "URL is null");
+		Numbers.inRange(URL.length(), TwitterUserRegistration.MIN_URL_LENGTH, TwitterUserRegistration.MAX_URL_LENGTH, "URL "+URL+" length "+URL.length()+" is not in range "+TwitterUserRegistration.MIN_URL_LENGTH+" -> "+TwitterUserRegistration.MAX_URL_LENGTH);
 		this.URL = URL;
 	}
 
@@ -159,14 +157,10 @@ public class TwitterUserRegistration extends SignedParticle
 		return this.description;
 	}
 
-	public void setDescription(String description) 
+	public void setDescription(final String description) 
 	{
-		if (Objects.requireNonNull(description).isEmpty() == true)
-			return;
-		
-		if (Objects.requireNonNull(description).length() > TwitterUserRegistration.MAX_DESCRIPTION_LENGTH)
-			throw new IllegalArgumentException("Description "+description+" length is greater than MAX_DESCRIPTION_LENGTH "+TwitterUserRegistration.MAX_DESCRIPTION_LENGTH);
-
+		Objects.requireNonNull(description, "Description is null");
+		Numbers.inRange(description.length(), TwitterUserRegistration.MIN_DESCRIPTION_LENGTH, TwitterUserRegistration.MAX_DESCRIPTION_LENGTH, "Description "+description+" length "+description.length()+" is not in range "+TwitterUserRegistration.MIN_DESCRIPTION_LENGTH+" -> "+TwitterUserRegistration.MAX_DESCRIPTION_LENGTH);
 		this.description = description;
 	}
 
@@ -175,11 +169,9 @@ public class TwitterUserRegistration extends SignedParticle
 		return this.numFollowers;
 	}
 
-	public void setNumFollowers(long numFollowers) 
+	public void setNumFollowers(final long numFollowers) 
 	{
-		if (numFollowers < 0)
-			throw new IllegalArgumentException("Followers can not be negative");
-		
+		Numbers.notNegative(numFollowers, "Followers can not be negative");
 		this.numFollowers = numFollowers;
 	}
 
@@ -190,9 +182,7 @@ public class TwitterUserRegistration extends SignedParticle
 
 	public void setNumFriends(long numFriends) 
 	{		
-		if (numFriends < 0)
-			throw new IllegalArgumentException("Friends can not be negative");
-
+		Numbers.notNegative(numFriends, "Friends can not be negative");
 		this.numFriends = numFriends;
 	}
 	
@@ -203,66 +193,49 @@ public class TwitterUserRegistration extends SignedParticle
 
 	public void setNumStatuses(long numStatuses) 
 	{		
-		if (numStatuses < 0)
-			throw new IllegalArgumentException("Status count can not be negative");
-
+		Numbers.notNegative(numStatuses, "Status count can not be negative");
 		this.numStatuses = numStatuses;
 	}
-
+	
 	@Override
-	public Set<Indexable> getIndexables()
+	public void prepare(final StateMachine stateMachine, final Object ... arguments) throws ValidationException, IOException 
 	{
-		Set<Indexable> indexables = super.getIndexables();
-		indexables.add(Indexable.from(this.handle, getClass()));
-		indexables.add(Indexable.from(getOwner().asHash(), getClass()));
-		return indexables;
-	}
-
-	@Override
-	public void prepare(StateMachine stateMachine) throws ValidationException, IOException 
-	{
-		if (this.handle == null)
-			throw new ValidationException("Handle is null");
+		Objects.requireNonNull(this.handle, "Handle is null");
+		Numbers.inRange(this.handle.length(), TwitterUserRegistration.MIN_HANDLE_LENGTH, TwitterUserRegistration.MAX_HANDLE_LENGTH, "Handle "+this.handle+" length "+this.handle.length()+" is not in range "+TwitterUserRegistration.MIN_HANDLE_LENGTH+" -> "+TwitterUserRegistration.MAX_HANDLE_LENGTH);
 		
-		if (this.handle.isEmpty() == true)
-			throw new ValidationException("Handle is empty");
+		Numbers.lessThan(this.createdAt, Universe.getDefault().getTimestamp(), "Created time "+this.createdAt+" is before genesis time "+Universe.getDefault().getTimestamp());
 
-		if (this.handle.length() > TwitterUserRegistration.MAX_SCREENNAME_LENGTH)
-			throw new ValidationException("Handle "+this.handle+" length "+this.handle.length()+" is greater than MAX_SCREENNAME_LENGTH "+TwitterUserRegistration.MAX_SCREENNAME_LENGTH);
-		
-		if (this.name != null && this.name.length() > TwitterUserRegistration.MAX_NAME_LENGTH)
-			throw new ValidationException("Name length "+this.name.length()+" is greater than MAX_NAME_LENGTH "+TwitterUserRegistration.MAX_NAME_LENGTH);
+		if (this.name != null)		
+			Numbers.inRange(this.name.length(), TwitterUserRegistration.MIN_NAME_LENGTH, TwitterUserRegistration.MIN_NAME_LENGTH, "Name "+this.name+" length "+this.name.length()+" is not in range "+TwitterUserRegistration.MIN_NAME_LENGTH+" -> "+TwitterUserRegistration.MAX_NAME_LENGTH);
 
-		if (this.createdAt <= Universe.getDefault().getTimestamp())
-			throw new ValidationException("Created time "+this.createdAt+" is before genesis time "+Universe.getDefault().getTimestamp());
-		
-		if (this.location != null && this.location.length() > TwitterUserRegistration.MAX_LOCATION_LENGTH)
-			throw new ValidationException("Location length "+this.location.length()+" is greater than MAX_LOCATION_LENGTH "+TwitterUserRegistration.MAX_LOCATION_LENGTH);
+		if (this.location != null)		
+			Numbers.inRange(this.location.length(), TwitterUserRegistration.MIN_LOCATION_LENGTH, TwitterUserRegistration.MAX_LOCATION_LENGTH, "Location "+this.location+" length "+this.location.length()+" is not in range "+TwitterUserRegistration.MIN_LOCATION_LENGTH+" -> "+TwitterUserRegistration.MAX_LOCATION_LENGTH);
 
-		if (this.URL != null && this.URL.length() > TwitterUserRegistration.MAX_URL_LENGTH)
-			throw new ValidationException("URL length "+this.URL.length()+" is greater than MAX_URL_LENGTH "+TwitterUserRegistration.MAX_URL_LENGTH);
+		if (this.URL != null)		
+			Numbers.inRange(this.URL.length(), TwitterUserRegistration.MIN_URL_LENGTH, TwitterUserRegistration.MAX_URL_LENGTH, "URL "+this.URL+" length "+this.URL.length()+" is not in range "+TwitterUserRegistration.MIN_URL_LENGTH+" -> "+TwitterUserRegistration.MAX_URL_LENGTH);
+
 		// TODO URL form verification
+		
+		if (this.description != null)
+			Numbers.inRange(this.description.length(), TwitterUserRegistration.MIN_DESCRIPTION_LENGTH, TwitterUserRegistration.MAX_DESCRIPTION_LENGTH, "Description "+this.description+" length "+this.description.length()+" is not in range "+TwitterUserRegistration.MIN_DESCRIPTION_LENGTH+" -> "+TwitterUserRegistration.MAX_DESCRIPTION_LENGTH);
 
-		if (this.numFriends < 0)
-			throw new ValidationException("Friends can not be negative");
-
-		if (this.numFollowers < 0)
-			throw new ValidationException("Followers can not be negative");
-
-		if (this.numStatuses < 0)
-			throw new ValidationException("Status count can not be negative");
+		Numbers.notNegative(this.numFriends, "Friends can not be negative");
+		Numbers.notNegative(this.numFollowers, "Followers can not be negative");
+		Numbers.notNegative(this.numStatuses, "Status count can not be negative");
+		
+		stateMachine.sop(new StateOp(new StateAddress(TwitterUserRegistration.class, Hash.from(UInt256.from(this.id))), Instruction.NOT_EXISTS), this);
+		stateMachine.sop(new StateOp(new StateAddress(TwitterUserRegistration.class, Hash.from(this.handle.toLowerCase())), Instruction.NOT_EXISTS), this);
+		stateMachine.sop(new StateOp(new StateAddress(TwitterUserRegistration.class, getOwner().asHash()), Instruction.NOT_EXISTS), this);
 		
 		super.prepare(stateMachine);
 	}
 
 	@Override
-	public void execute(StateMachine stateMachine) throws ValidationException, IOException 
+	public void execute(StateMachine stateMachine, Object ... arguments) throws ValidationException, IOException 
 	{
-		String user = stateMachine.get("user");
-		if (user == null)
-			stateMachine.put("user", this.handle);
-		else
-			throw new ValidationException("State machine already has user "+user+" specified");
+		stateMachine.sop(new StateOp(new StateAddress(TwitterUserRegistration.class, Hash.from(UInt256.from(this.id))), UInt256.from(this.id), Instruction.SET), this);
+		stateMachine.sop(new StateOp(new StateAddress(TwitterUserRegistration.class, Hash.from(this.handle.trim().toLowerCase())), UInt256.from(Hash.from(this.handle.trim().toLowerCase()).toByteArray()), Instruction.SET), this);
+		stateMachine.sop(new StateOp(new StateAddress(TwitterUserRegistration.class, getOwner().asHash()), UInt256.from(getOwner().asHash().toByteArray()), Instruction.SET), this);
 	}
 	
 	@Override

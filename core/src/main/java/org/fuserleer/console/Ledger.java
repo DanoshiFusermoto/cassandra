7f6618cf2ce6f4e995bd39c2d1303bf12364f25b
Dan Hughes <dan@radixdlt.com>
2021-04-25 07:12:34 +0100

    Snapshot verbosity and served shard identifier

--- core/src/main/java/org/fuserleer/console/Ledger.java
+++ core/src/main/java/org/fuserleer/console/Ledger.java
@@ -9,6 +9,7 @@ import org.apache.commons.cli.Options;
 import org.fuserleer.Context;
 import org.fuserleer.crypto.Hash;
 import org.fuserleer.ledger.Block;
+import org.fuserleer.ledger.ShardMapper;
 import org.fuserleer.ledger.StateKey;
 import org.fuserleer.time.Time;
 
@@ -50,32 +51,37 @@ public class Ledger extends Function
 		}
 		else if (commandLine.hasOption("snapshot") == true)
 		{
+			boolean verbose = false;
+			String option = commandLine.getOptionValue("snapshot");
+			if (option != null && option.compareToIgnoreCase("verbose") == 0)
+				verbose = true;
+			
 			Collection<Hash> atomHandlerPending = context.getLedger().getAtomHandler().pending();
-			atomHandlerPending.forEach(p -> printStream.println(p.toString()));
-			printStream.println(atomHandlerPending.size()+" pending in atom handler");
+			if (verbose) atomHandlerPending.forEach(p -> printStream.println(p.toString()));
+			printStream.println(atomHandlerPending.size()+" pending in atom handler "+atomHandlerPending.stream().reduce((a, b) -> Hash.from(a,b)));
 
 			Collection<Hash> atomPoolPending = context.getLedger().getAtomPool().pending();
-			atomPoolPending.forEach(p -> printStream.println(p.toString()));
-			printStream.println(atomPoolPending.size()+" pending in atom pool");
+			if (verbose) atomPoolPending.forEach(p -> printStream.println(p.toString()));
+			printStream.println(atomPoolPending.size()+" pending in atom pool "+atomPoolPending.stream().reduce((a, b) -> Hash.from(a,b)));
 
 			Collection<Hash> stateHandlerPending = context.getLedger().getStateHandler().pending();
-			stateHandlerPending.forEach(p -> printStream.println(p.toString()));
-			printStream.println(stateHandlerPending.size()+" pending in state handler");
+			if (verbose) stateHandlerPending.forEach(p -> printStream.println(p.toString()));
+			printStream.println(stateHandlerPending.size()+" pending in state handler "+stateHandlerPending.stream().reduce((a, b) -> Hash.from(a,b)));
 			
 			Collection<Hash> statePoolPending = context.getLedger().getStatePool().pending();
-			statePoolPending.forEach(p -> printStream.println(p.toString()));
-			printStream.println(statePoolPending.size()+" pending in state pool");
+			if (verbose) statePoolPending.forEach(p -> printStream.println(p.toString()));
+			printStream.println(statePoolPending.size()+" pending in state pool "+statePoolPending.stream().reduce((a, b) -> Hash.from(a,b)));
 			
 			Collection<Hash> stateAccumulatorLocked = context.getLedger().getStateAccumulator().locked();
-			stateAccumulatorLocked.forEach(p -> printStream.println(p.toString()));
-			printStream.println(stateAccumulatorLocked.size()+" locked in accumulator");
+			if (verbose) stateAccumulatorLocked.forEach(p -> printStream.println(p.toString()));
+			printStream.println(stateAccumulatorLocked.size()+" locked in accumulator "+stateAccumulatorLocked.stream().reduce((a, b) -> Hash.from(a,b)));
 
 			printStream.println("Current head: "+context.getLedger().getHead());
 		}
 		else
 		{
 			printStream.println("Synced: "+context.getLedger().isSynced());
-			printStream.println("Identity: "+context.getNode().getIdentity());
+			printStream.println("Identity: S-"+(ShardMapper.toShardGroup(context.getNode().getIdentity(), context.getLedger().numShardGroups()))+" <- "+context.getNode().getIdentity());
 			printStream.println("Current head: "+context.getLedger().getHead());
 			printStream.println("Ledger timestamp: "+Time.getLedgerTimeSeconds()+" / "+new Date(Time.getLedgerTimeMS())); // TODO only accurate for simulated time
 			printStream.println("Atoms (P/L/T): "+context.getLedger().getAtomHandler().numPending()+"/"+context.getMetaData().get("ledger.processed.atoms.local", 0l)+"/"+context.getMetaData().get("ledger.processed.atoms.total", 0l));

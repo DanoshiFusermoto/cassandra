2203d1a4bc53136cce25fa48d9cf988b38bca910
Dan Hughes <dan@radixdlt.com>
2021-05-14 06:59:55 +0100

    Default getHash function added to Hashable interface

--- core/src/main/java/org/fuserleer/crypto/Hashable.java
+++ core/src/main/java/org/fuserleer/crypto/Hashable.java
@@ -1,6 +1,26 @@
 package org.fuserleer.crypto;
 
+import org.fuserleer.crypto.Hash.Mode;
+import org.fuserleer.serialization.DsonOutput;
+import org.fuserleer.serialization.Serialization;
+import org.fuserleer.serialization.DsonOutput.Output;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public interface Hashable
 {
-	public Hash getHash();
+	@JsonProperty("hash")
+	@DsonOutput(Output.API)
+	public default Hash getHash()
+	{
+		try
+		{
+			byte[] hashBytes = Serialization.getInstance().toDson(this, Output.HASH);
+			return new Hash(hashBytes, Mode.DOUBLE);
+		}
+		catch (Exception e)
+		{
+			throw new RuntimeException("Error generating hash: " + e, e);
+		}
+	}
 }
